1466f352aea15dea3b73befb113a687de17c5d0a
Arturo Grau <arturo.grau@koahealth.com>
2024-06-05 18:11:08 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.treasure_hunter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:treasure_hunter/app/app.dart';
+import 'package:treasure_hunter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
