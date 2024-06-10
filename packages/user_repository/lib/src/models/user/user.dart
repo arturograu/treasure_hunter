83b221caa61e418c441069db3b10952295a40566
Arturo Grau <arturo.grau@koahealth.com>
2024-06-10 17:48:26 +0300

    feat: create domain layer

---
+++ packages/user_repository/lib/src/models/user/user.dart
@@ -0,0 +1,24 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:treasure_hunter_api_client/treasure_hunter_api_client.dart'
+    as api_client;
+import 'package:treasures_repository/treasures_repository.dart';
+
+part 'user.freezed.dart';
+
+@freezed
+abstract class User with _$User {
+  const factory User({
+    required String id,
+    required String name,
+    required String email,
+    required List<Treasure> favouriteTreasures,
+  }) = _User;
+
+  factory User.fromApi(api_client.User userApi) => User(
+        id: userApi.id,
+        name: userApi.name,
+        email: userApi.email,
+        favouriteTreasures:
+            userApi.favouriteTreasures.map(Treasure.fromApi).toList(),
+      );
+}
---
+++ packages/user_repository/lib/src/user_repository.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:treasure_hunter_api_client/treasure_hunter_api_client.dart'
+    as api_client;
+import 'package:treasures_repository/treasures_repository.dart';
+import 'package:user_repository/src/models/user/user.dart';
+
+/// User controller
+typedef UserController = StreamController<User>;
+
+class UserRepository {
+  UserRepository({
+    required api_client.TreasureHunterApiClient apiClient,
+  }) : _apiClient = apiClient;
+
+  final api_client.TreasureHunterApiClient _apiClient;
+
+  /// Stream of [api_client.User] which will emit the current user when the user changes
+  /// in the data layer.
+  Stream<User> get user => _userController.stream;
+
+  final _userController = UserController.broadcast();
+
+  /// Change the user's name.
+  ///
+  /// [name] - The new name.
+  Future<void> changeName(String name) async {
+    final user = await _apiClient.updateUser(name: name);
+    _userController.add(User.fromApi(user));
+  }
+
+  /// Change the user's email.
+  ///
+  /// [email] - The new email.
+  Future<void> changeEmail(String email) async {
+    final user = await _apiClient.updateUser(email: email);
+    _userController.add(User.fromApi(user));
+  }
+
+  /// Add a treasure to the user's favourite treasures.
+  ///
+  /// [treasureId] - The ID of the treasure to add.
+  Future<void> markTreasureAsFavourite(String treasureId) async {
+    await _apiClient.addUserFavouriteTreasure(treasureId);
+    final user = await _apiClient.fetchCurrentUser();
+    _userController.add(User.fromApi(user));
+  }
+
+  /// Add a treasure to the user's collection of treasures.
+  ///
+  /// [treasureId] - The ID of the treasure to add.
+  Future<List<Treasure>> addTreasure(String treasureId) async {
+    final userTreasures = await _apiClient.addUserTreasure(treasureId);
+    return userTreasures.map(Treasure.fromApi).toList();
+  }
+}
